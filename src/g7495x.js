3c217b5dfc0a78cd8a03ffbbf4a391d4d624d4a8
G7495x <ghanashyam74@gmail.com>
2019-02-17 20:21:24 +0530

    gitlab -> github

--- src/g7495x.js
+++ src/g7495x.js
@@ -1,6 +1,6 @@
 const g7495x=(aidText='By')=>{
 	const href='https://twitter.com/g7495x'
-	const source='https://github.com/G7495x/WebGL-G7495x-Signature---Three.js'
+	const source='https://g7495x.github.io/WebGL-G7495x-Signature---Three.js/src'
 
 	const piBy180=Math.PI/180
 	const twoPi=Math.PI*2
